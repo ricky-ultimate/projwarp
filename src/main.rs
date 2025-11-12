78ccb4008900b88ba050df0366fd6d7ace3d51c5
Ricky Ultimate <rickyogigi@gmail.com>
2025-11-13 00:09:59 +0100

    feat: main entry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod cli;
+mod config;
+mod actions;
+mod fuzzy;
+
+use cli::Cli;
+use clap::Parser;
+use actions::handle_action;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    if let Err(e) = handle_action(args) {
+        eprintln!("Error: {e}");
+    }
 }
