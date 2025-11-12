3e49049f7f49c1fb6694b3053cb3b870e239f61f
Ricky Ultimate <rickyogigi@gmail.com>
2025-11-13 00:08:38 +0100

    feat: define cli commands

---
+++ src/cli.rs
@@ -0,0 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "projwarp")]
+#[command(author = "リッキー")]
+#[command(version = "0.1")]
+#[command(about = "Jump to projects quickly", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Add the current directory as a project
+    Add {
+        #[arg(short, long)]
+        alias: Option<String>,
+    },
+    /// List all known projects
+    List,
+    /// Jump to a project directory (prints path to stdout)
+    Go {
+        name: String,
+        /// Open in VS Code instead of cd
+        #[arg(long)]
+        code: bool,
+    },
+}
