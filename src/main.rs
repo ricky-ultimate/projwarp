7b47371307e8aa3baba37e0fa49ae0c9c476f812
Ricky Ultimate <rickyogigi@gmail.com>
2025-11-13 07:42:18 +0100

    feat: add install mod

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+mod actions;
 mod cli;
 mod config;
-mod actions;
 mod fuzzy;
+mod installer;
 
 use cli::Cli;
 use clap::Parser;
 use actions::handle_action;
 
+
 fn main() {
     let args = Cli::parse();
     if let Err(e) = handle_action(args) {
         eprintln!("Error: {e}");
+        std::process::exit(1);
     }
 }
